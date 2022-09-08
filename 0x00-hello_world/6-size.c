1f86cad9b6f29ca9bcfe4d55942afa22a046b9fd
amine <aminealouani2018@gmail.com>
2022-09-08 08:03:27 -0700

    Task 6 done.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ char charType;
 float floatType;
 long int longIntType;
 long long int long2IntType;
-printf("Size of a char: %zu byte(s)\n", sizeof(charType));
-printf("Size of an int: %zu byte(s)\n", sizeof(intType));
-printf("Size of a long int: %zu byte(s)\n", sizeof(longIntType));
-printf("Size of a long long int: %zu byte(s)\n", sizeof(long2IntType));
-print("Size of a float: %zu byte(s)", sizeof(floatType));
+printf("Size of a char: %i byte(s)\n", sizeof(charType));
+printf("Size of an int: %i byte(s)\n", sizeof(intType));
+printf("Size of a long int: %i byte(s)\n", sizeof(longIntType));
+printf("Size of a long long int: %i byte(s)\n", sizeof(long2IntType));
+print("Size of a float: %i byte(s)", sizeof(floatType));
 return (0);
 }
