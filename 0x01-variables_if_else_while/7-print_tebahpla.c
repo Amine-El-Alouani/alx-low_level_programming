1ae76b6dfa012bd9f4e78fe10db463d8d9b43bc1
amine <aminealouani2018@gmail.com>
2022-09-09 10:24:32 -0700

    Task 7 done.

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Smile in the mirror
+ * Return: 0
+ */
+
+
+int main(void)
+{
+int x;
+	for(x = 'z'; x >= 'a'; x--)
+	{
+		putchar(x);
+	}
+putchar('\n');
+return (0);
+}
