73a867ee895c87e3e92192b90db4213cce4238de
amine <aminealouani2018@gmail.com>
2022-09-09 09:44:16 -0700

    Task 5 done.

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,18 +1,2 @@
 #include <stdio.h>
 
-/**
- * main - main block
- * Numbers
- * Return: 0
- */
-
-int main(void)
-{
-int nums;
-	for(nums = 0; nums < 10; nums++)
-	{
-		printf("%d", nums);
-	}
-printf("\n");
-return (0);
-}
