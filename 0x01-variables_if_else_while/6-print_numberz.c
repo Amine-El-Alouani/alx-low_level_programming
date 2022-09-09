d9cc571c4e791c229fe5d566204f8e9e4e3994a8
amine <aminealouani2018@gmail.com>
2022-09-09 10:11:23 -0700

    Task 6 done.

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main Block
+ * Numberz
+ * Return: 0
+ */
+
+int main(void)
+{
+int nz;
+	for (nz = '0'; nz < '10'; nz++)
+	{
+		putchar(nz);
+	}
+putchar('\n');
+return (0);
+}
