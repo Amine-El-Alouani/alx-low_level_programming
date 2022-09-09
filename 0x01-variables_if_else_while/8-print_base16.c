f7d47149903854d54b238a0ea39129ea6ac3c374
amine <aminealouani2018@gmail.com>
2022-09-09 10:36:09 -0700

    Task 8 done.

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+int n, ch;
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+putchar('\n');
+return (0);
+}
