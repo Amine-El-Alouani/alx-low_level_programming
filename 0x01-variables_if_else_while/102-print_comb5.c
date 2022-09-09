4f93a8ef49fd97f06af570744659f993a80b3c28
amine <aminealouani2018@gmail.com>
2022-09-09 16:22:38 -0700

    Task 12 done

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,12 +7,12 @@
 
 int main(void)
 {
-int i,j;
+int i, j;
 	for (i = 0; i <= 99; i++)
 	{
 		for (j = 0; j <= 99; j++)
 		{
-			if(i < j)
+			if (i < j)
 			{
 				putchar((i / 10) + 48);
 				putchar((i % 10) + 48);
@@ -25,7 +25,7 @@ int i,j;
 					putchar(' ');
 				}
 			}
-		}	
+		}
 	}
 	putchar('\n');
 	return (0);
