d2338bcf93db1da761b5ddd4aa4ef0b68d7bbb20
amine <aminealouani2018@gmail.com>
2022-09-09 07:56:25 -0700

    Task 2 done.

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,18 +2,16 @@
 #include <ctype.h>
 /**
  * main - main block
- * 
  * Return: 0
  */
 
 int main(void)
-{	
-int x;
-
-for(x = 'A'; x <='Z'; x++) 
 {
-	int lowerX = tolower(x);
-	putchar(lowerX);
+char x;
+for (x = 'A'; x <= 'Z'; x++)
+{
+	x = tolower(x);
+	putchar(x);
 }
 return 0;
 }
