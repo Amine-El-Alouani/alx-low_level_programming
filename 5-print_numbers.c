a31f7e59db0e32fa236d0be357ac2e70fb2284b1
amine <aminealouani2018@gmail.com>
2022-09-09 09:40:49 -0700

    Task 5 done.

---
+++ 5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+int nums;
+	for(nums = 0; nums < 10; nums++)
+	{
+		printf("%d", nums);
+	}
+printf("\n");
+return (0);
+}
