bfe2311ce25323f645a54ff63ccd1255cbf7763a
amine <aminealouani2018@gmail.com>
2022-09-13 17:30:19 -0700

    Task 10 done.

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,11 +3,13 @@
  * add - add 2 int
  * @n1: number 1
  * @n2: number 2
- * Retutn: Result
+ * Description: results
+ * Return: Result
  */
 
 int add(int n1, int n2)
 {
 	int result = n1 + n2;
-	return (result);
+	
+return (result);
 }
