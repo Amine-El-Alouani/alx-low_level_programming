c0e9d62f629d4ffd4379cbd704abf6ec68f52b45
amine <aminealouani2018@gmail.com>
2022-09-15 09:56:38 -0700

    Task 2 done.

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,9 +4,11 @@
  * mul - multiply nums
  * @a: num one
  * @b: num two
+ * Description: check val
  * Return value
  */
 int mul(int a, int b)
 {
-	return (a * b);
+	int result = a * b;
+	return (result);
 }
