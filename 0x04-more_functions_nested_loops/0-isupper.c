277b208b581e8ef49de6feb14e314d2d7ad6cc9a
amine <aminealouani2018@gmail.com>
2022-09-15 09:13:28 -0700

    Task 0 done.

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - check uppercase
+ * @c: char to check
+ * Description: check upper char
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+{
+	return (c <= 90 && c >= 65);
+}
