5d6c777c8d43043504aaec8939cfd81c35722c66
amine <aminealouani2018@gmail.com>
2022-09-16 16:38:54 -0700

    Task 11 done.

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	unsigned int long n = 612852475143, a = (int) sqrt(n);
+unsigned int long f = 612852475143, g = (int) sqrt(n);
 	while (1)
 	{
-		if (n % a == 0)
+		if (f % g == 0)
 		{
 			printf("%lu \n", n / a);
 			break;
