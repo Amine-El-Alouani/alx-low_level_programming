c753d5227e766663ee528f50f2ca5b2dc94c3f52
amine <aminealouani2018@gmail.com>
2022-09-16 16:18:10 -0700

    Task 10 done.

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * print_triangles - func of printing triangles
+ * @size: input value
+ * Description: Triangles
+ * Return: none
+ */
+
+void print_triangle(int size)
+{
+	int i, n;
+	int z = size - 1;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (n = 0; n < size; n++)
+			{
+				if (j < z)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar('#');
+				}
+			}
+			z--;
+			_putchar('\n');
+		}
+	}
+	
+}
