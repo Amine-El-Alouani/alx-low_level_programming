dc54e15399528468d57432cbe47287773797348f
amine <aminealouani2018@gmail.com>
2022-09-16 14:09:13 -0700

    Task 5 done.

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,16 +6,18 @@
  */
 void more_numbers(void)
 {
-int i, n;
+
+	int i, n;
+
 	for (n = 0; n <= 10; n++)
 	{
 		for (i = 0; i <= 14; i++)
 		{
 			if (i >= 10)
 			{
-				_putchar((i / 10) + '0');
+				_putchar(i / 10 + '0');
 			}
-			_putchar((i % 10) + '0');
+			_putchar(i % 10 + '0');
 		}
 		_putchar('\n');
 	}
