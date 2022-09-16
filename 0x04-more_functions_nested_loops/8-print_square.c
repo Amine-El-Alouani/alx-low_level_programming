597ecbaff57aa80f4ba861bb5536ba0ad0e09bf8
amine <aminealouani2018@gmail.com>
2022-09-16 14:30:50 -0700

    Task 8 done.

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_square - func print
+ * @size: input val
+ * Description: printing squares
+ * Return: none
+ */
+void print_square(int size)
+{
+	int i, p;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	for (i = 0; i < size; i++)
+	{
+		for (p = 0; p < size: p++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
