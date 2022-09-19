15b62ce727ba249e8ffb220b11e4603b15504034
amine <aminealouani2018@gmail.com>
2022-09-19 04:41:40 -0700

    Task 0 done.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - reset value to 98
+ * @n: input value
+ * Description: reset value
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
