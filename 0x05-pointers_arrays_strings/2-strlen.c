338768cc191821855a6ab83163b50e53c745d942
amine <aminealouani2018@gmail.com>
2022-09-19 08:55:44 -0700

    Task 2 done.

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - string length
+ * @s: input value
+ * Description: calcul string length
+ * Return: nothing
+*/
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return i;
+}
