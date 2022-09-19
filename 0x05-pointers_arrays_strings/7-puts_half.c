fc522610058cb34a5f7db659276f45ebac83fe2b
amine <aminealouani2018@gmail.com>
2022-09-19 16:50:42 -0700

    Task 7 done.

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * puts_half - void function
+ * @str: input value
+ * Description: prints the second half of string
+ * Return: nothing
+ */
+
+void puts_half(char *str)
+{
+	int len = 0;
+	int i;
+	int n;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	if ((len % 2) != 0)
+	{
+		n = (len - 1) / 2;
+		for (i = n; i < len; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		n = len / 2;
+		for (i = n; i < len; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
