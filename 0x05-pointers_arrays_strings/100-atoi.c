836e926749783e046715c3fd430394d8d91755d6
amine <aminealouani2018@gmail.com>
2022-09-20 03:35:35 -0700

    Task 11 done.

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - int function
+ * @s: input value
+ * Description: convert string to integer
+ * Return: 0
+ */
+
+int _atoi(char *s)
+{
+	
+}
