715c246ecf734e03cd82423bdc82590d0657840b
amine <aminealouani2018@gmail.com>
2022-09-20 15:32:21 -0700

    Task 11 done.

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * _atoi - int function
@@ -10,5 +9,36 @@
 
 int _atoi(char *s)
 {
-	
+	int num = 0;
+	int len = 0;
+	int i;
+	int sin = 1;
+	int digit = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		if (s[i] == '-')
+		{
+			sin *= -1;
+		}
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = 1;
+			num = num * 10 + (s[i] - '0');
+		}
+		else if (digit)
+		{
+			break;
+		}
+	}
+	if (sin < 0)
+	{
+		num = -num;
+	}
+	return (num);
 }
