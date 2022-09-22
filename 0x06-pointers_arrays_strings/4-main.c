322a614e870dd4b1c2aff3c177a4807c7edb3b83
amine <aminealouani2018@gmail.com>
2022-09-22 15:28:12 -0700

    Task 3 done.

---
+++ 0x06-pointers_arrays_strings/4-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (i != 0)
+		{
+			printf(", ");
+		}
+		printf("%d", a[i]);
+		i++;
+	}
+	print("\n");
+}
