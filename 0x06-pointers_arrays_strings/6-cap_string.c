0aa0f7a9eb5408c459da0ba09785ef62ff120e06
amine <aminealouani2018@gmail.com>
2022-09-23 04:22:20 -0700

    Task 6 done.

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * cap_string - char function
+ * @c: input value
+ * Description: cap string
+ * Return: value
+ */
+
+char *cap_string(char *s)
+{
+	int i, j, f;
+	char s_chars[] = {' ', '\n', '\t', '.', ';', '!', '?', '"', '(', ',', ')', '{', '}', };
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; j < i; j++)
+	{
+		for (f = 0; f < 13; f++)
+		{
+			if (s[j] == s_chars[f]){	
+				if (s[j + 1] >= 'a' && s[j + 1] <= 'z')
+				{
+					s[j + 1] -= 32;
+				}
+			}
+		}
+	}
+	return (s);
+}
