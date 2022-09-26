9737e442294ac187d8041835a3b76171527f3cb8
amine <aminealouani2018@gmail.com>
2022-09-26 15:40:02 -0700

    Task 2 done.

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strchr - char func
+ * _strchr - char funcition
  * @s: input one
  * @c: input two
  * Description: locates a character in a string
@@ -12,7 +12,7 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; i >= '\0'; i++)
+	for (i = 0; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
 		{
