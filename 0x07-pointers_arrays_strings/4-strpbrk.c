6aa5470316fb96109afd0b26289672a905f7ea13
amine <aminealouani2018@gmail.com>
2022-09-27 10:48:58 -0700

    Task 4 done.

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -15,7 +15,7 @@ char *_strpbrk(char *s, char *accept)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (f = 0; accept[f] !='\0'; f++)
+		for (f = 0; accept[f] != '\0'; f++)
 		{
 			if (s[i] == accept[f])
 			{
