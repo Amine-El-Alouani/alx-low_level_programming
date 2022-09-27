7cf1a58b84ec25b4c519a7f73902b861bcb47e7b
amine <aminealouani2018@gmail.com>
2022-09-27 16:54:34 -0700

    Task 6 done.

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - void func
+ * @a: input one
+ * @size: input two
+ * Description: prints the sum of the two diagonals
+ * Return: value
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, sum1 = 0, sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[i];
+		a += size;
+	}
+	for (i = 0; i < size; i++)
+	{
+		a -= size;
+		sum2 += a[i];
+	}
+	printf("%d, %d\n", sum1, sum2)
+}
