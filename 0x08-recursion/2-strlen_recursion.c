631eac8ded795eff842e72a579809da930f98c44
amine <aminealouani2018@gmail.com>
2022-09-28 07:51:33 -0700

    Task 2 done.

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - int func
+ * @s: input one
+ * Return: int
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		return(_strlen_recursion(s + 1) + 1);
+	}
+	return (0);
+}
