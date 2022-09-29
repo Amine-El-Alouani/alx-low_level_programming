60d4fbebef1a40a500d4d7f36cea8c6a72a69f97
amine <aminealouani2018@gmail.com>
2022-09-29 13:08:41 -0700

    Task 7 done.

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * last - int func
+ * @s: input
+ * Return: value
+ */
+
+int last(char *s)
+{
+	int n = 0;
+
+	if (*s > '\0')
+	{
+		n += last(s + 1) + 1;
+	}
+	return (n);
+}
+
+/**
+ * is_palindrome - int func
+ * @s: input
+ * Return: 1 or 0;
+ */
+
+int is_palindrome(char *s)
+{
+	int e = last(s);
+
+	return (check(s, 0, end - 1, e % 2));
+}
+
+/**
+ * check - int func
+ * @s: input one
+ * @str: input two
+ * @end: input three
+ * @p: input four
+ * Return: 0 or 1
+ */
+
+int check(char *s, int str, int end, int p)
+{
+	if ((str == end && p != 0) || (str == end + 1 && p == 0))
+	{
+		return (1);
+	}
+	else if (s[str] != s[end])
+	{
+		return (0);
+	}
+	else
+	{
+		return (check(s, str + 1, end - 1, p));
+	}
+}
