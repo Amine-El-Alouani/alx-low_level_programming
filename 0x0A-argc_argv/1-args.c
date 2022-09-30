cc19c89d04cd5a00606979d079ceb5da68bb0efa
amine <aminealouani2018@gmail.com>
2022-09-30 03:22:54 -0700

    Task 1 done.

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - int func
+ * @argc: input one
+ * @argv: input two
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	(void)argv;
+	return (0);
+}
