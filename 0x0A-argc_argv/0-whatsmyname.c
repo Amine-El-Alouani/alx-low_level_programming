a4097d92bf398d09c7681a529222af3a594405e2
amine <aminealouani2018@gmail.com>
2022-09-30 03:12:34 -0700

    Task 0 done

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - int func
+ * @argc: input 1
+ * @argv: input 2
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 1)
+	{
+		printf("%s\n", argv[argc - 1]);
+	}
+	return (0);
+}
