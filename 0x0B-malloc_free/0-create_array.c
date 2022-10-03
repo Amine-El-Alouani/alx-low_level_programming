e79614a0f7d8e04a30df00bab58570818c394812
amine <aminealouani2018@gmail.com>
2022-10-03 10:39:13 -0700

    Task 0 done.

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,12 +17,13 @@ char *create_array(unsigned int size, char c)
 	{
 		s[i] = c;
 	}
-	if (size == 0 || s == 0)
+	if (size == 0)
 	{
 		return (NULL);
 	}
-	else
+	if (s == NULL)
 	{
-		return (s);
+		return (NULL);
 	}
+	return (s);
 }
