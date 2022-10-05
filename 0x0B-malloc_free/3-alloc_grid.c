ce40b9593b8de9d83715d318e76e8b05a494fb2f
amine <aminealouani2018@gmail.com>
2022-10-05 03:06:48 -0700

    Task 3 done.

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - int func
+ * @width: input
+ * @height: input
+ * Return: value or NULL
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **grid, i, j;
+
+	grid = malloc(height * sizeof(*grid));
+
+	if (width <= 0 || height <= 0 || grid == 0)
+	{
+		return (NULL);
+	}	
+	else
+	{
+		for (i = 0; i < height; i++)
+		{
+			grid[i] = malloc(width * sizeof(**grid));
+			if (grid[i] == 0)
+			{
+				while (i--)
+				{
+					free(grid[i]);
+				}
+				free(grid);
+				return (NULL);
+			}
+			for (j = 0; j < width; j++)
+			{
+				grid[i][j] = 0;
+			}
+		}
+	}
+	return (grid);
+}
