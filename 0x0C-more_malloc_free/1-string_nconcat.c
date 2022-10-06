72b90d2e29b4b15f3c45943ffdba30962fb23348
amine <aminealouani2018@gmail.com>
2022-10-06 16:39:49 -0700

    Task 1 done.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,33 +18,27 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s1 = "";
 	}
-
 	if (s2 == NULL)
 	{
 		s2 = "";
 	}
-
 	while (s1[i] != '\0')
 	{
 		i++;
 	}
-
 	while (s2[j] != '\0')
 	{
 		j++;
-	}
-	
+	}	
 	if (n >= j)
 	{
 		n = j;
 	}
 	s = malloc(sizeof(char) * (i + n) + 1);
-
 	if (s == NULL)
 	{
 		return (NULL);
 	}
-
 	for (f = 0; f < i + n; f++)
 	{
 		if (f < i)
