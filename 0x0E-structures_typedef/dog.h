0b2493496196b4ead60f892261e7f6bd060324fc
amine <aminealouani2018@gmail.com>
2022-10-10 16:50:46 -0700

    Task 3 done.

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,14 +13,9 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-typedef struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-} dog_t;
 
 #endif
