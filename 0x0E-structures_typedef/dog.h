7d4837bafc28f4dd1530144c6cb1bbe990793761
amine <aminealouani2018@gmail.com>
2022-10-10 16:02:26 -0700

    Task 0 done.

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,15 @@
 #ifndef DOG_STRUCT
 #define DOG_STRUCT
 
-struct dog {
+/**
+ * struct dog - struct
+ * @name: var
+ * @age: var
+ * @owner: var
+ * Return: value
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
