a5de1a5c66e217c2442bd026dcc8efd5fad04260
amine <aminealouani2018@gmail.com>
2022-10-21 17:03:24 -0700

    Task 4 done.

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_list - void func
+ * @head: param
+ * Return: nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *ptr;
+
+	while (head != NULL)
+	{
+		ptr = head;
+		head = head->next;
+		free(ptr);
+	}
+}
